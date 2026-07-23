50fc2733640aafa627f8c413dfc1ffdd522d3aaf
tribe <tribeiro.tr33@gmail.com>
2026-07-23 17:42:57 -0300

    Atualiza chave publica VAPID (par de chaves regenerado)

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import { Tasks, Events, PushSubscriptions, TaskOccurrences } from './supabase-client.js';
 import { chooseVaultFolder, getSavedVaultFolder, scanVaultTasks, writeTaskDoneBackToFile } from './fs-obsidian.js';
 
-const VAPID_PUBLIC_KEY = 'BMjdUFwgjj7MenVtoGCcXuhx2G_ZVH1lWggHAyK_QmJsGSBdGGqsU79ag4-lMqoi9lJNZmmszRM62heV7YOUKjY';
+const VAPID_PUBLIC_KEY = 'BAPUfQl6z7y6oxsKp8QdaiiurMvqhBlA549o6T3M2gN0k1zzODZfa1FY2T2I6sBdAc3rkFkbIZXv7VPMWPgLbY8';
 const WEEKDAY_LABELS = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
 
 let vaultHandle = null;
